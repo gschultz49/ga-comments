0aa4dd2c6b9695cffc18336fbc2ed5d7ee76fde3
Greg Schultz <gschultz49@gmail.com>
2020-12-06 15:00:45 -0500

    working tailwind installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-// import Test from "./comp";
 import "./App.css";
 
 import firebase from "firebase/app";
--- src/Components/ClassList.tsx
+++ src/Components/ClassList.tsx
@@ -7,17 +7,18 @@ export interface ClassCard {
 }
 
 const ClassList = ({ teacherId }: { teacherId: string | undefined }) => {
+  console.log(teacherId);
   const [classes, loading, error] = useCollectionData<ClassCard>(
     firebase.firestore().collection("classes").where("uid", "==", teacherId),
     {
       snapshotListenOptions: { includeMetadataChanges: true },
     }
   );
+  console.log(classes);
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-4">
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Loading...</span>}
-
       {classes?.map((c) => (
         <ClassCardComponent name={c.name} uid={c.uid} />
       ))}
@@ -33,5 +34,10 @@ const ClassCardComponent = ({ name, uid }: ClassCard) => {
     </div>
   );
 };
+
+const CreateClassCard = () => {
+
+
+}
 
 export default ClassList;
