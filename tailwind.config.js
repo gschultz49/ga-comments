e4ed4142bc49d36ee1ba049e8d455ae2a37aca6d
Greg Schultz <gschultz49@gmail.com>
2020-12-06 15:47:28 -0500

    reconfigure postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 // tailwind.config.js
 module.exports = {
-    purge: ['./src/**/*.js', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  purge: ['./src/**/*.tsx', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
