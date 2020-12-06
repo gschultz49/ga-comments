762e10cb9de9641a4a18b4bc813f724749c50613
Greg Schultz <gschultz49@gmail.com>
2020-12-06 15:30:36 -0500

    clsx and basic click event

--- src/Components/ClassList.tsx
+++ src/Components/ClassList.tsx
@@ -1,5 +1,6 @@
 import firebase from "firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import clsx from "clsx";
 
 export interface ClassCard {
   name: string;
@@ -16,9 +17,10 @@ const ClassList = ({ teacherId }: { teacherId: string | undefined }) => {
   );
   console.log(classes);
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-3 gap-6">
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Loading...</span>}
+      <CreateClassCard teacherId={teacherId} />
       {classes?.map((c) => (
         <ClassCardComponent name={c.name} uid={c.uid} />
       ))}
@@ -26,18 +28,45 @@ const ClassList = ({ teacherId }: { teacherId: string | undefined }) => {
   );
 };
 
-const ClassCardComponent = ({ name, uid }: ClassCard) => {
+const CardWrapper = ({ children, onClick, styles = [] }: any) => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <h1>{uid}</h1>
+    <div
+      className={clsx([
+        "shadow-lg",
+        "rounded-md",
+        "bg-purple-200",
+        "cursor-pointer",
+        "transform",
+        "transition",
+        "hover:scale-110",
+        "motion-reduce:transform-none",
+        ...styles,
+      ])}
+      onClick={onClick}
+    >
+      {children}
     </div>
   );
 };
 
-const CreateClassCard = () => {
+const ClassCardComponent = ({ name, uid }: ClassCard) => {
+  return (
+    <CardWrapper>
+      <h1>{name}</h1>
+      <h1>{uid}</h1>
+    </CardWrapper>
+  );
+};
 
-
-}
+const CreateClassCard = ({ teacherId }: { teacherId: string | undefined }) => {
+  return (
+    <CardWrapper
+      onClick={(e: Event) => console.log("clicked add card", teacherId, e)}
+      styles={[]}
+    >
+      <html>create class button</html>
+    </CardWrapper>
+  );
+};
 
 export default ClassList;
