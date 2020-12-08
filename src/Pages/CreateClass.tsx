7d934ac47ab30799cd8ab0e6f6c37ed130fe591a
Greg Schultz <gschultz49@gmail.com>
2020-12-07 23:46:47 -0500

    add in formik validation

--- src/Pages/CreateClass.tsx
+++ src/Pages/CreateClass.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../App";
+// import * as Yup from "yup";
+import { useFormik } from "formik";
 
 const CreateClass = () => {
   const [user] = useAuthState(auth);
@@ -101,71 +103,159 @@ const StudentRow = ({
 
 const FIRST_NAME_DEFAULT = "";
 const LAST_NAME_DEFAULT = "";
-const GENDER_DEFAULT = "Male";
+const GENDER_DEFAULT = "-1";
+
+const validate = (values: any) => {
+  const errors: any = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length < 2) {
+    errors.firstName = "Must be 2 characters or greater";
+  }
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  } else if (values.lastName.length < 2) {
+    errors.lastName = "Must be 2 characters or greater";
+  }
+
+  if (values.gender == GENDER_DEFAULT) {
+    errors.gender = "Select Gender";
+  }
+
+  return errors;
+};
+
+export const InlineError = ({ text }: { text: string }) => (
+  <i className="text-red-700">{text}</i>
+);
 
 const AddStudent = ({
   setStudents,
 }: {
   setStudents: React.Dispatch<React.SetStateAction<Student[]>>;
 }) => {
-  const [firstName, setFirstName] = useState(FIRST_NAME_DEFAULT);
-  const [lastName, setLastName] = useState(LAST_NAME_DEFAULT);
-  const [gender, setGender] = useState(GENDER_DEFAULT);
+  //   const [firstName, setFirstName] = useState(FIRST_NAME_DEFAULT);
+  //   const [lastName, setLastName] = useState(LAST_NAME_DEFAULT);
+  //   const [gender, setGender] = useState(GENDER_DEFAULT);
+  const formik = useFormik({
+    initialValues: {
+      firstName: FIRST_NAME_DEFAULT,
+      lastName: LAST_NAME_DEFAULT,
+      gender: GENDER_DEFAULT,
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   const [isEditable, setIsEditable] = useState(false);
   const styles = ["grid", "grid-cols-4", "gap-4"];
   if (isEditable) {
     return (
-      <div className={clsx(styles)}>
+      <form onSubmit={formik.handleSubmit} className={clsx(styles)}>
         <div>
+          {/* <label htmlFor="firstName">First Name</label> */}
           <input
+            id="firstName"
+            name="firstName"
             type="text"
-            onChange={(e) => setFirstName(e.currentTarget.value)}
-            value={firstName}
             placeholder="First Name"
-          ></input>
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.firstName}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div><InlineError text={formik.errors.firstName}/></div>
+          ) : null}
         </div>
         <div>
+          {/* <label htmlFor="lastName">Last Name</label> */}
           <input
+            id="lastName"
+            name="lastName"
             type="text"
-            onChange={(e) => setLastName(e.currentTarget.value)}
-            value={lastName}
             placeholder="Last Name"
-          ></input>
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.lastName}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <div> <InlineError text={formik.errors.lastName}/></div>
+          ) : null}
         </div>
         <div>
-          <select
-            value={gender}
-            onChange={(e) => setGender(e.currentTarget.value)}
-          >
-            <option selected value="Male">
-              Male
-            </option>
-            <option value="Female">Female</option>
-            <option value="Other">Other</option>
-          </select>
+          <div>
+            <select
+              id={"gender"}
+              name={"gender"}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            >
+              <option value="-1">Select...</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+            {formik.touched.gender && formik.errors.gender ? (
+              <div><InlineError text={formik.errors.gender}/></div>
+            ) : null}
+          </div>
         </div>
-
-        <div
-          onClick={(e) => {
-            setStudents((prevStudents) => [
-              ...prevStudents,
-              {
-                firstName,
-                lastName,
-                gender,
-                id: `${Math.random()}`,
-              },
-            ]);
-
-            setFirstName(FIRST_NAME_DEFAULT);
-            setLastName(LAST_NAME_DEFAULT);
-            setGender(GENDER_DEFAULT);
-            // setIsEditable(false);
-          }}
-        >
-          <button>Add button</button>
+        <div>
+          <button type="submit">Submit</button>
         </div>
-      </div>
+      </form>
+
+      //   <div className={clsx(styles)}>
+      //     <div>
+      //       <input
+      //         type="text"
+      //         onChange={(e) => setFirstName(e.currentTarget.value)}
+      //         value={firstName}
+      //         placeholder="First Name"
+      //       ></input>
+      //     </div>
+      //     <div>
+      //       <input
+      //         type="text"
+      //         onChange={(e) => setLastName(e.currentTarget.value)}
+      //         value={lastName}
+      //         placeholder="Last Name"
+      //       ></input>
+      //     </div>
+      //     <div>
+      //       <select
+      //         value={gender}
+      //         onChange={(e) => setGender(e.currentTarget.value)}
+      //       >
+      //         <option value="-1">Select...</option>
+      //         <option value="Male">Male</option>
+      //         <option value="Female">Female</option>
+      //         <option value="Other">Other</option>
+      //       </select>
+      //     </div>
+
+      //     <div
+      //       onClick={(e) => {
+      //         setStudents((prevStudents) => [
+      //           ...prevStudents,
+      //           {
+      //             firstName,
+      //             lastName,
+      //             gender,
+      //             id: `${Math.random()}`,
+      //           },
+      //         ]);
+
+      //         setFirstName(FIRST_NAME_DEFAULT);
+      //         setLastName(LAST_NAME_DEFAULT);
+      //         setGender(GENDER_DEFAULT);
+      //         // setIsEditable(false);
+      //       }}
+      //     >
+      //       <button>Add button</button>
+      //     </div>
+      //   </div>
     );
   } else {
     return (
