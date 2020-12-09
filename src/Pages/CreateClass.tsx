55e955a9da6813f460c989d01cf66bc9eb2cd400
Greg Schultz <gschultz49@gmail.com>
2020-12-09 01:19:57 -0500

    cleaning

--- src/Pages/CreateClass.tsx
+++ src/Pages/CreateClass.tsx
@@ -1,30 +1,9 @@
 import clsx from "clsx";
-import React from "react";
-import { useState } from "react";
+import { ErrorMessage, Field, FieldArray, Form, Formik } from "formik";
+import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../App";
 import * as Yup from "yup";
-import {
-  ErrorMessage,
-  Field,
-  FieldArray,
-  Form,
-  Formik,
-  FormikProps,
-  getIn,
-  useFormik,
-} from "formik";
-
-const ErrorMessageCustom = ({ name }: any) => (
-  <Field
-    name={name}
-    render={({ form }: any) => {
-      const error = getIn(form.errors, name);
-      const touch = getIn(form.touched, name);
-      return touch && error ? error : null;
-    }}
-  />
-);
+import { auth } from "../App";
 
 const CreateClass = () => {
   const [user] = useAuthState(auth);
@@ -50,7 +29,8 @@ const CreateClass = () => {
       isNew: true,
     },
   ];
-  const [students, setStudents] = useState<Student[]>(studentsData);
+  const [students] = useState<Student[]>(studentsData); // come from some network request
+  const [submission, setSubmission] = useState<Student[]>([]);
   return (
     <section>
       <section>
@@ -72,6 +52,7 @@ const CreateClass = () => {
           <Formik
             initialValues={{ students: students }}
             onSubmit={async (values) => {
+              setSubmission([...values.students]);
               console.log(values);
               //   await new Promise((r) => setTimeout(r, 500));
               //   alert(JSON.stringify(values, null, 2));
@@ -86,42 +67,20 @@ const CreateClass = () => {
                       {values.students.map((student: Student, index) => {
                         if (student.isNew && student.isNew === true) {
                           return (
-                            <div className={clsx(styles)} key={index}>
+                            <div className={clsx(styles)} key={student.id}>
                               <div>
-                                <Field name={`students[${index}].firstName`}>
-                                  {/* {({
-                                      field, // { name, value, onChange, onBlur }
-                                      form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-                                      meta,
-                                    }: any) => (
-                                      <div>
-                                        <input type="text" placeholder="First name" {...field} />
-                                        {meta.touched && meta.error && (
-                                          <div className="error">{meta.error}</div>
-                                        )}
-                                      </div>
-                                    )} */}
-                                </Field>
+                                <Field
+                                  name={`students[${index}].firstName`}
+                                ></Field>
                                 <ErrorMessage
                                   name={`students[${index}].firstName`}
                                   render={(msg) => <InlineError text={msg} />}
                                 />
                               </div>
                               <div>
-                                <Field name={`students[${index}].lastName`}>
-                                  {/* {({
-                                      field, // { name, value, onChange, onBlur }
-                                      form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-                                      meta,
-                                    }: any) => (
-                                      <div>
-                                        <input type="text" placeholder="last name" {...field} />
-                                        {meta.touched && meta.error && (
-                                          <div className="error">{meta.error}</div>
-                                        )}
-                                      </div>
-                                    )} */}
-                                </Field>
+                                <Field
+                                  name={`students[${index}].lastName`}
+                                ></Field>
                                 <ErrorMessage
                                   name={`students[${index}].lastName`}
                                   render={(msg) => <InlineError text={msg} />}
@@ -147,12 +106,6 @@ const CreateClass = () => {
                           );
                         } else {
                           return (
-                            // <StudentRow
-                            //   key={`${student.firstName}-${student.lastName}`}
-                            //   student={student}
-                            //   setStudents={setStudents}
-                            // />
-
                             <div className="grid grid-cols-4 gap-4">
                               <div>{student.firstName}</div>
                               <div>{student.lastName}</div>
@@ -175,12 +128,13 @@ const CreateClass = () => {
                     </div>
                   )}
                 </FieldArray>
-                {/* <button type="submit">Invite</button> */}
               </Form>
             )}
           </Formik>
         </div>
       </section>
+      <h1>results:</h1>
+      <pre>{JSON.stringify(submission, undefined, 2)}</pre>
     </section>
   );
 };
@@ -191,157 +145,10 @@ export interface Student {
   gender: string;
   isNew?: boolean;
 }
-// https://stackoverflow.com/questions/55075740/property-does-not-exist-on-type-intrinsicattributes
-// const StudentRows = ({
-//   students,
-//   setStudents,
-// }: {
-//   students: Student[];
-//   setStudents: React.Dispatch<React.SetStateAction<Student[]>>;
-// }) => {
-//   return (
-//     <React.Fragment>
-//       {students.map((student: Student) => {
-//         if (student.isNew && student.isNew === true) {
-//           return (
-//             <div className={clsx(styles)}>
-//               <div>
-//                 <Field name={`students.${$index}firstName`}>
-//                   {/* {({
-//                         field, // { name, value, onChange, onBlur }
-//                         form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-//                         meta,
-//                       }: any) => (
-//                         <div>
-//                           <input type="text" placeholder="First name" {...field} />
-//                           {meta.touched && meta.error && (
-//                             <div className="error">{meta.error}</div>
-//                           )}
-//                         </div>
-//                       )} */}
-//                 </Field>
-//                 <ErrorMessage
-//                   name="firstName"
-//                   render={(msg) => <InlineError text={msg} />}
-//                 />
-//               </div>
-//               <div>
-//                 <Field name="lastName">
-//                   {/* {({
-//                         field, // { name, value, onChange, onBlur }
-//                         form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-//                         meta,
-//                       }: any) => (
-//                         <div>
-//                           <input type="text" placeholder="last name" {...field} />
-//                           {meta.touched && meta.error && (
-//                             <div className="error">{meta.error}</div>
-//                           )}
-//                         </div>
-//                       )} */}
-//                 </Field>
-//                 <ErrorMessage
-//                   name="lastName"
-//                   render={(msg) => <InlineError text={msg} />}
-//                 />
-//               </div>
-
-//               <div>
-//                 <Field as="select" name="gender">
-//                   <option value="">Select...</option>
-//                   <option value="Male">Male</option>
-//                   <option value="Female">Female</option>
-//                   <option value="Other">Other</option>
-//                 </Field>
-//                 <ErrorMessage
-//                   name="gender"
-//                   render={(msg) => <InlineError text={msg} />}
-//                 />
-//               </div>
-//               {/* <div>
-//                   <button type="submit">Submit</button>
-//                 </div> */}
-//             </div>
-//           );
-//         } else {
-//           return (
-//             <StudentRow
-//               key={`${student.firstName}-${student.lastName}`}
-//               student={student}
-//               setStudents={setStudents}
-//             />
-//           );
-//         }
-//       })}
-//     </React.Fragment>
-//   );
-// };
-// const StudentRow = ({
-//   student,
-//   setStudents,
-// }: {
-//   student: Student;
-//   setStudents: React.Dispatch<React.SetStateAction<Student[]>>;
-// }) => {
-//   const { firstName, lastName, gender, id } = student;
-//   return (
-//     <div className="grid grid-cols-4 gap-4">
-//       <div>{firstName}</div>
-//       <div>{lastName}</div>
-//       <div>{gender}</div>
-//       <div
-//         onClick={(e) => {
-//           setStudents((prevStudents) =>
-//             prevStudents.filter((student) => student.id !== id)
-//           );
-//         }}
-//       >
-//         <button>remove button</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-const FIRST_NAME_DEFAULT = "";
-const LAST_NAME_DEFAULT = "";
-const GENDER_DEFAULT = "";
-
-// const validate = (values: any) => {
-//   const errors: any = {};
-//   if (!values.firstName) {
-//     errors.firstName = "Required";
-//   } else if (values.firstName.length < 2) {
-//     errors.firstName = "Must be 2 characters or greater";
-//   }
-//   if (!values.lastName) {
-//     errors.lastName = "Required";
-//   } else if (values.lastName.length < 2) {
-//     errors.lastName = "Must be 2 characters or greater";
-//   }
-
-//   if (values.gender == GENDER_DEFAULT) {
-//     errors.gender = "Select Gender";
-//   }
-
-//   return errors;
-// };
 
 export const InlineError = ({ text }: { text: string }) => (
   <i className="text-red-700">{text}</i>
 );
-
-// const NewStudentSchema = Yup.object().shape({
-//   firstName: Yup.string()
-//     .min(2, "Too Short!")
-//     .max(70, "Too Long!")
-//     .required("Required"),
-//   lastName: Yup.string()
-//     .min(2, "Too Short!")
-//     .max(70, "Too Long!")
-//     .required("Required"),
-//   gender: Yup.string().ensure().required("Required"),
-//   //   email: Yup.string().email("Invalid email").required("Required"),
-// });
 
 const NestedStudentSchema = Yup.object().shape({
   students: Yup.array().of(
@@ -355,79 +162,14 @@ const NestedStudentSchema = Yup.object().shape({
         .max(70, "Too Long!")
         .required("Required"),
       gender: Yup.string().ensure().required("Required"),
-      //   email: Yup.string().email("Invalid email").required("Required"),
     })
   ),
 });
+const FIRST_NAME_DEFAULT = "";
+const LAST_NAME_DEFAULT = "";
+const GENDER_DEFAULT = "";
 
 const styles = ["grid", "grid-cols-4", "gap-4"];
-
-// const AddStudent = ({
-//   setStudents,
-// }: {
-//   setStudents: React.Dispatch<React.SetStateAction<Student[]>>;
-// }) => {
-//   return (
-//     <div className={clsx(styles)}>
-//       <div>
-//         <Field name="firstName">
-//           {/* {({
-//                     field, // { name, value, onChange, onBlur }
-//                     form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-//                     meta,
-//                   }: any) => (
-//                     <div>
-//                       <input type="text" placeholder="First name" {...field} />
-//                       {meta.touched && meta.error && (
-//                         <div className="error">{meta.error}</div>
-//                       )}
-//                     </div>
-//                   )} */}
-//         </Field>
-//         <ErrorMessage
-//           name="firstName"
-//           render={(msg) => <InlineError text={msg} />}
-//         />
-//       </div>
-//       <div>
-//         <Field name="lastName">
-//           {/* {({
-//                     field, // { name, value, onChange, onBlur }
-//                     form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-//                     meta,
-//                   }: any) => (
-//                     <div>
-//                       <input type="text" placeholder="last name" {...field} />
-//                       {meta.touched && meta.error && (
-//                         <div className="error">{meta.error}</div>
-//                       )}
-//                     </div>
-//                   )} */}
-//         </Field>
-//         <ErrorMessage
-//           name="lastName"
-//           render={(msg) => <InlineError text={msg} />}
-//         />
-//       </div>
-
-//       <div>
-//         <Field as="select" name="gender">
-//           <option value="">Select...</option>
-//           <option value="Male">Male</option>
-//           <option value="Female">Female</option>
-//           <option value="Other">Other</option>
-//         </Field>
-//         <ErrorMessage
-//           name="gender"
-//           render={(msg) => <InlineError text={msg} />}
-//         />
-//       </div>
-//       {/* <div>
-//               <button type="submit">Submit</button>
-//             </div> */}
-//     </div>
-//   );
-// };
 
 const AddStudentButton = ({ push }: { push: any }) => {
   return (
